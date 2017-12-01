fe064d504543997b7ba76fb1096424609e395d0e
Anson Melakayil <anson.melakayil@yahoo.com>
2017-12-01 02:19:33 +0000

    comment test pre test push

--- spec/factories.rb
+++ spec/factories.rb
@@ -10,6 +10,8 @@ FactoryBot.define do
 
 	factory :gram do
 		message "hello"
+		picture { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png'), 'image/png') }
 		association :user
 	end
+
 end
